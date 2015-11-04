265e934a5cc28edc5e766c3b429dd93c5dd46dfa
ckehoe <clayton.kehoe@gmail.com>
2015-11-04 20:24:48 +0000

    adding some more tests for apache

--- cookbooks/apache/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/apache/test/integration/default/serverspec/default_spec.rb
@@ -6,4 +6,22 @@ describe 'apache::default' do
   describe package('httpd') do
     it { should be_installed }
   end
+
+  describe file('/var/www/html/index.html') do
+    it { should be_file }
+  end
+
+  describe file('/var/www/html/index.html') do
+    its(:content) { should match /Hello/ } 
+  end
+
+  # Check that the webserver is running
+  describe port(80) do
+    it { should be_listening }
+  end
+
+  # Check that the webserver is accepting traffic
+  describe command('curl localhost') do
+    its(:exit_status) { should eq 0 }
+  end
 end
