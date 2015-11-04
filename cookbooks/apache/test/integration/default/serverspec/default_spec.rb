f82fc3b097c9bc0a4b0b29ed26e611546ef37ce9
ckehoe <clayton.kehoe@gmail.com>
2015-11-04 20:02:55 +0000

    adding tests using kitcen

--- cookbooks/apache/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/apache/test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'apache::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  describe package('httpd') do
+    it { should be_installed }
   end
 end
